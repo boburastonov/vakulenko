b594772eb6529cecb51c3bb51434d68b7f4b7529
boburastonov <boburastonov33@gmail.com>
2024-07-29 09:46:03 +0500

    add results section to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/navbar";
 import Image from "next/image";
 import bg from '../assets/hero-bg.jpeg'
 import Hero from "@/components/hero";
+import Results from "@/components/results";
 
 export default function Home() {
   return (
